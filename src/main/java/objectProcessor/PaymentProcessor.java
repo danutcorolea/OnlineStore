60e36c84e909eb66b6a6c870870925387c9f6dee
danut <danutcorolea@gmail.com>
2018-08-19 17:44:04 +0300

    update

---
+++ src/main/java/objectProcessor/PaymentProcessor.java
@@ -0,0 +1,38 @@
+package objectProcessor;
+
+import com.opencsv.CSVReader;
+import dataObjects.Payment;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PaymentProcessor {
+
+    public List<Payment> parseCSVFileLineByLine() throws IOException {
+
+        CSVReader reader = new CSVReader(new FileReader("C:\\Users\\danut\\Desktop\\CSVfiles\\Carts.txt"), ',');
+
+        List<Payment> payments = new ArrayList<>();
+        String[] record = null;
+
+        reader.readNext();
+
+        while((record = reader.readNext())!= null){
+
+            Payment payment = new Payment();
+            payment.setIdPayment(Integer.parseInt(record[0]));
+            payment.setIdCart(Integer.parseInt(record[1]));
+            payment.setIdCustomer(Integer.parseInt(record[2]));
+            payment.setStatus(record[3]);
+            payment.setType(record[4]);
+            payment.setAmmount(Double.parseDouble(record[5]));
+            payments.add(payment);
+        }
+
+        reader.close();
+
+        return payments;
+    }
+}
