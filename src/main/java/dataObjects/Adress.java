549a1d63c5c9f2ecb2f52a959e73bda061a369c3
danut <danutcorolea@gmail.com>
2018-08-22 16:11:27 +0300

    update

--- src/main/java/dataObjects/Adress.java
+++ src/main/java/dataObjects/Adress.java
@@ -2,29 +2,21 @@ package dataObjects;
 
 public class Adress {
 
-    private int idAdress;
+
     private int idCustomer;
     private String country;
     private String locality;
     private String street;
     private int streetNumber;
 
+    public Adress(){}
 
-    public Adress(int idAdress, int idCustomer, String country, String locality, String street, int streetNumber) {
-        this.idAdress = idAdress;
+    public Adress(int idCustomer, String country, String locality, String street, int streetNumber) {
         this.idCustomer = idCustomer;
         this.country = country;
         this.locality = locality;
         this.street = street;
         this.streetNumber = streetNumber;
-    }
-
-    public int getIdAdress() {
-        return idAdress;
-    }
-
-    public void setIdAdress(int idAdress) {
-        this.idAdress = idAdress;
     }
 
     public int getIdCustomer() {
@@ -70,8 +62,7 @@ public class Adress {
     @Override
     public String toString() {
         return "Adress{" +
-                "idAdress=" + idAdress +
-                ", idCustomer=" + idCustomer +
+                "idCustomer=" + idCustomer +
                 ", country='" + country + '\'' +
                 ", locality='" + locality + '\'' +
                 ", street='" + street + '\'' +
