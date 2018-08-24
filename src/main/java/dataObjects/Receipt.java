ba57668f12173492f48ac023b8a00f58f20df128
danut <danutcorolea@gmail.com>
2018-08-24 13:57:50 +0300

    update

--- src/main/java/dataObjects/Receipt.java
+++ src/main/java/dataObjects/Receipt.java
@@ -5,26 +5,50 @@ import databaseManager.DBManager;
 
 import java.sql.Connection;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Receipt {
 
-    private int idRecceipt;
+    private static  final AtomicInteger count = new AtomicInteger(0);
+    private int idReceipt;
     private int idCustomer;
     private int idCart;
-    List<Purchase> purchases;
     double totalPayment;
 
+    List<Purchase> purchases;
 
-    public Receipt(){};
 
+    public Receipt(){
 
-    public Receipt(int idCart, List<Purchase> purchases, double totalPayment) {
+    }
+
+    public Receipt(int idReceipt,int idCart, List<Purchase> purchases, double totalPayment) {
+        this.idReceipt = count.incrementAndGet();
         this.idCart = idCart;
         this.purchases = purchases;
         this.totalPayment = totalPayment;
     }
 
+    public int getIdCart() {
+        return idCart;
+    }
+
+    public List<Purchase> getPurchases() {
+        return purchases;
+    }
+
+    public double getTotalPayment() {
+        return totalPayment;
+    }
+
+    public int getIdReceipt() {
+        return idReceipt;
+    }
+
+
+
     public Receipt generateReceipt(int cartID)throws Exception {
+
 
         DBManager db1 = new DBManager();
         Connection connection1 = DBConnection.getConnection();
@@ -32,17 +56,19 @@ public class Receipt {
         Payment payment = new Payment();
         totalPayment = payment.computeTotalAmmountOfPayments(db1.getPaymentByCart(cartID,connection1));
 
-        Receipt receipt = new Receipt(cartID,purchases,totalPayment);
+        Receipt receipt = new Receipt(idReceipt, cartID,purchases,totalPayment);
 
         return receipt;
     }
 
+
+
     @Override
     public String toString() {
-        return "Receipt with " +
-                "CartID" + idCart +
-                ", contains " + purchases +
-                ", having a total of" + totalPayment +
+        return "Receipt{" +
+                "idCart=" + idCart +
+                ", purchases=" + purchases +
+                ", totalPayment=" + totalPayment +
                 '}';
     }
 }
